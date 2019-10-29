f3a7637ba37912e1e37af75f6ccb774c98cc7a75
Taiki Watanabe <taiki.w.1113@gmail.com>
2019-10-29 17:16:20 +0900

    送信文面の改良

--- timetable.py
+++ timetable.py
@@ -10,7 +10,7 @@ def get_timetable(postback):
     count = 1
     day = postback[-3:]
     for subject in jsn[day].items():
-        text += '【' + subject[0] + '時間目】\n' + subject[1] + '\n'
+        text += '【' + subject[0] + '時間目】\n' + subject[1] + '\n\n'
         if count == len(jsn[day]):
             text += '\n' + subject[0][-1] + '時間授業です。'
         count += 1
